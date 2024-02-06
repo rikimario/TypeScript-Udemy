6f26b315ccc2c242806c9beab8cc9928a1cd0ee8
rikimario <mario_martinov@abv.bg>
2024-02-06 23:11:35 +0200

    working with numbers, strings and booleans

--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
 }
 
 const number1 = 6;
-const number2 = 2.5;
+const number2 = 3;
+const printResult = true;
+const printPhrase = "Result is: ";
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, printPhrase);
