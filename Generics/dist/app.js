2a83df73a1719f1a7504d334f106e6563a156a57
rikimario <mario_martinov@abv.bg>
2024-02-08 23:47:48 +0200

    generic types

--- Generics/dist/app.js
+++ Generics/dist/app.js
@@ -1,1 +1,10 @@
 "use strict";
+const names = [];
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(10);
+    }, 2000);
+});
+// promise.then((data) => {
+//   data.split(" ");
+// });
---
+++ Generics/src/app.ts
@@ -0,0 +1,11 @@
+const names: Array<string> = [];
+
+const promise: Promise<number> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(10);
+  }, 2000);
+});
+
+// promise.then((data) => {
+//   data.split(" ");
+// });
