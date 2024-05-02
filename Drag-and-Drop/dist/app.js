2a11d2ef3559d938d80a88503238a365de7ce17f
rikimario <mario_martinov@abv.bg>
2024-05-02 18:33:27 +0300

    filtering projects with enum

--- Drag-and-Drop/dist/app.js
+++ Drag-and-Drop/dist/app.js
@@ -93,7 +93,13 @@ class ProjectList {
         this.element = importedNode.firstElementChild;
         this.element.id = `${this.type}-projects`;
         projectState.addListener((projects) => {
-            this.assignedProjects = projects;
+            const relevanceProjects = projects.filter((prj) => {
+                if (this.type === "active") {
+                    return prj.status === ProjectStatus.Active;
+                }
+                return prj.status === ProjectStatus.Finished;
+            });
+            this.assignedProjects = relevanceProjects;
             this.renderProject();
         });
         this.attach();
@@ -101,6 +107,7 @@ class ProjectList {
     }
     renderProject() {
         const listEl = document.getElementById(`${this.type}-projects-list`);
+        listEl.innerHTML = "";
         for (const prjItem of this.assignedProjects) {
             const listItem = document.createElement("li");
             listItem.textContent = prjItem.title;
